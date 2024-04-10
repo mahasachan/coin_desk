771e62daf85caa9d7dbf2afd692b76f844137bd8
mahasachan <mahasachan.sa@ku.th>
2024-04-10 09:42:46 +0700

    add theme App

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {}
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 119, 176, 170),
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Coin Desk',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-    );
+        title: 'Coin Desk',
+        theme: ThemeData().copyWith(
+            colorScheme: kColorScheme,
+            appBarTheme: AppBarTheme(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer,
+            )));
   }
 }
